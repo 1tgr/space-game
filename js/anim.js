153a751825284e56118b2a5e3d2672b8e7ef8a57
Tim Robinson <tim.g.robinson@gmail.com>
2015-01-19 19:49:33 +0000

    wrapInput, interpolation

--- js/anim.js
+++ js/anim.js
@@ -1,10 +1,16 @@
 "use strict";
 (function(window) {
-  function Interpolator(startValue, endValue, seconds, atEnd) {
-    var self = this;
-    var speed = 1 / (seconds || 1);
-    self.t = ko.observable(0);
+  function linear1D(startValue, endValue, t) {
+    return (1 - t) * startValue + t * endValue;
+  }
 
+  function Animator(startValue, endValue, options) {
+    var self = this;
+    var speed = 1 / (options.seconds || 1);
+    var atEnd = options.atEnd;
+    var func = options.func || linear1D;
+
+    self.t = ko.observable(0);
     self.startTime = new Date();
 
     self.animate = function (dt) {
@@ -17,17 +23,29 @@
         atEnd();
         return endValue;
       } else {
-        return (1 - t) * startValue + t * endValue;
+        return func(startValue, endValue, t);
       }
     });
   }
 
   window.anim = { };
+
+  window.anim.linear1D = linear1D;
+
+  window.anim.linear2D = function(startPos, endPos, t) {
+    return {
+      x: linear1D(startPos.x, endPos.x, t),
+      y: linear1D(startPos.y, endPos.y, t)
+    };
+  };
+
   window.anim.animator = function(owner) {
+    var interpolators = { };
+
     function anim(name, commit, seconds) {
-      var interps = anim.interpolators();
-      var interp = interps[name];
-      var obsBefore = interp ? interp.value : owner[name];
+      var animators = anim.animators();
+      var animator = animators[name];
+      var obsBefore = animator ? animator.value : owner[name];
       if (!commit) {
         return obsBefore;
       }
@@ -36,22 +54,47 @@
       commit();
 
       var valueAfter = owner[name]();
-      interps[name] = new Interpolator(valueBefore, valueAfter, seconds, function () {
-        var interps = anim.interpolators.peek();
-        delete interps[name];
-        anim.interpolators(interps);
+      animators[name] = new Animator(valueBefore, valueAfter, {
+        seconds: seconds,
+        func: interpolators[name],
+        atEnd: function () {
+          var animators = anim.animators.peek();
+          delete animators[name];
+          anim.animators(animators);
+        }
       });
 
-      anim.interpolators(interps);
+      anim.animators(animators);
       return undefined;
     }
 
-    anim.interpolators = ko.observable({ });
+    anim.animators = ko.observable({ });
 
     anim.animate = function(dt) {
-      $.each(anim.interpolators(), function(_, interp) {
-        interp.animate(dt);
+      $.each(anim.animators(), function(_, animator) {
+        animator.animate(dt);
       });
+    };
+
+    anim.wrapInput = function(name1, name2) {
+      return ko.computed({
+        read: owner[name1],
+        write: function(value) {
+          anim(name2, function() {
+            owner[name1](value);
+          });
+        }
+      });
+    };
+
+    anim.interpolator = function(name, func) {
+      if (func) {
+        interpolators[name] = func;
+        return undefined;
+      }
+      else {
+        return interpolators[name];
+      }
     };
 
     return anim;
